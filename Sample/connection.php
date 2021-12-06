33e197cd8867e59449c8d51ed00677b3e31269c2
AnanyaAgarwal <56514921+AnanyaAgarwal@users.noreply.github.com>
2021-12-06 16:25:19 +0530

    Update connection.php

--- Sample/connection.php
+++ Sample/connection.php
@@ -1,8 +1,8 @@
 <?php
-$hostname="aa1s3tjurcz7qh3.cjobsuyviyes.ap-south-1.rds.amazonaws.com" ; //local server name default localhost
+$hostname="aayvh0ta5yeicf.cjobsuyviyes.ap-south-1.rds.amazonaws.com" ; //local server name default localhost
 $username="AnanyaAg";  //mysql username default is root.
 $password="Mrawyad...16";       //blank if no password is set for mysql.
-$database="aa1s3tjurcz7qh3";  //database name which you created
+$database="aayvh0ta5yeicf";  //database name which you created
 $con=mysql_connect($hostname,$username,$password);
 if(! $con)
 {
